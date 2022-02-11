af212ef8bd583e3fa6f72e526fd4d042494eea5d
Max Kutovoy <maxkutovoy@yandex.ru>
2022-02-11 12:53:55 +0600

    Добавил отправку сообщений о проверенных работах в телеграм

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     polling_url = 'https://dvmn.org/api/user_reviews/'
 
     devman_token = os.getenv('DEVMAN_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    chat_id = os.getenv('CHAT_ID')
+
+    bot = telegram.Bot(token=telegram_token)
+    # bot.send_message(text='Hi', chat_id=chat_id)
 
     headers = {
         f'Authorization': f'Token {devman_token}'
@@ -25,6 +30,11 @@ def main():
 
     #last_timestamp = time.time()
     last_timestamp = timestamp_with_work
+
+    text_result = {
+        False: 'Все хорошо, можно приступать к следующему уроку',
+        True: 'Еще есть над чем поработать',
+    }
 
     while True:
         payload = {
@@ -42,6 +52,22 @@ def main():
 
             if last_attempt['status'] == 'found':
                 last_timestamp = last_attempt['last_attempt_timestamp']
+                for lesson in last_attempt['new_attempts']:
+                    result = text_result[lesson['is_negative']]
+                    lesson_url = lesson["lesson_url"]
+
+                    text = (
+                        'Преподаватель проверил вашу работу \n'
+                        f'"{lesson["lesson_title"]}" \n\n'
+                        f'{result} \n\n'
+                        f'<a href="{lesson_url}">Перейти к уроку</a> '
+                    )
+
+                    bot.send_message(
+                        text=text,
+                        chat_id=chat_id,
+                        parse_mode='HTML'
+                    )
 
         except requests.exceptions.ReadTimeout:
             print('Works not found')
